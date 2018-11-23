58c723efadef2d293fab2acc59178aebbd4f3aea
Bruno Lottero <brunoelottero@gmail.com>
2018-11-23 09:47:00 -0300

    Agrego reset a pc

--- pc.vhd
+++ pc.vhd
@@ -3,19 +3,21 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
 
 entity pc is
-  port(clk: in std_logic;
+  port(clk,rst: in std_logic;
        s: out std_logic_vector(6 downto 0));
 end pc;
 
 architecture pc_beh of pc is
 
--- quizas sea mejor implementar un reset, por ahora inicializamos en 0x00
-signal s_buf: std_logic_vector(6 downto 0) := "0000000";
+signal s_buf: std_logic_vector(6 downto 0);
 
 begin
-  process(clk)
+  process(clk, rst)
   begin
-    if (rising_edge(clk)) then
+    if (rst='1') then
+      s_buf <= "0000000";
+      s <= s_buf;
+    elsif (rising_edge(clk)) then
       s_buf <= std_logic_vector(unsigned(s_buf) + "1");
       s <= s_buf;
     end if;
--- pc_tb.vhd
+++ pc_tb.vhd
@@ -7,18 +7,21 @@ end pc_tb;
 architecture pc_test of pc_tb is
 
   component pc is
-    port(clk: in std_logic;
+    port(clk,rst: in std_logic;
          s: out std_logic_vector(6 downto 0));
   end component;
 
-  signal clk: std_logic;
+  signal clk,rst: std_logic;
   signal s: std_logic_vector(6 downto 0);
 
 begin
   clk1: entity work.clock(clock_architecture) port map(clk => clk);
-  uut: entity work.pc(pc_beh) port map(clk => clk, s => s);
+  uut: entity work.pc(pc_beh) port map(clk => clk, rst => rst, s => s);
   process
   begin
+    rst <= '1';
+    wait for 10 ns;
+    rst <= '0';
     wait for 100 ns;
     wait;
   end process;
