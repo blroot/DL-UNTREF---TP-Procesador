6338a10c56770ce257c5ca99aaec66c2d2d8050f
Matias Chavez <43224027+chavezma@users.noreply.github.com>
2018-12-01 16:31:41 -0300

    Update pc.vhd
    Cambio dimension de vector, es de 8 bits

--- pc.vhd
+++ pc.vhd
@@ -4,18 +4,18 @@ use IEEE.numeric_std.all;
 
 entity pc is
   port(clk,rst: in std_logic;
-       s: out std_logic_vector(6 downto 0));
+       s: out std_logic_vector(7 downto 0));
 end pc;
 
 architecture pc_beh of pc is
 
-signal s_buf: std_logic_vector(6 downto 0);
+signal s_buf: std_logic_vector(7 downto 0);
 
 begin
   process(clk, rst)
   begin
     if (rst='1') then
-      s_buf <= "0000000";
+      s_buf <= "00000000";
       s <= s_buf;
     elsif (rising_edge(clk)) then
       s_buf <= std_logic_vector(unsigned(s_buf) + "1");
